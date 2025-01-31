19eda85679b3da436cf552646a7bd68e7d86d7b7
Jeff Hall <jhall@jgiquality.com>
2025-01-30 20:59:00 -0600

    Save tests

---
+++ tests/test_upload.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Rename the file back to its original name
+mv "C:\Users\JGI\Downloads\Form_3018_Rockwell_2025-01-30.xlsm" "C:\Users\JGI\Downloads\Form_3018_Rockwell.xlsm"
+
+# Run the validate_upload.sh script
+bash upload_sop.sh
