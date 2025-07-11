eab55dbc7a0ba0e8dc68a2d1ec6b86b58cbe4428
Jeff Hall <rhythmatician5@gmail.com>
2025-07-11 15:28:09 -0500

    Remove old .sh tests

--- tests/test_validate_upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Rename the file back to its original name
-mv "C:\Users\JGI\Downloads\Form_3018_Rockwell_2025-01-31.xlsm" "C:\Users\JGI\Downloads\Form_3018_Rockwell.xlsm"
-
-# Run the validate_upload.sh script
-bash "C:\Users\JGI\Desktop\ci-scripts\validate_upload.sh"
