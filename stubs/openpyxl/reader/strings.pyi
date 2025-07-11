3f0a4f2e2a0c212850697663773203743f773f81
Jeff Hall <rhythmatician5@gmail.com>
2025-07-11 15:26:58 -0500

    Remove stubs completely

--- stubs/openpyxl/reader/strings.pyi
+++
@@ -1,7 +0,0 @@
-from openpyxl.cell.rich_text import CellRichText as CellRichText
-from openpyxl.cell.text import Text as Text
-from openpyxl.xml.constants import SHEET_MAIN_NS as SHEET_MAIN_NS
-from openpyxl.xml.functions import iterparse as iterparse
-
-def read_string_table(xml_source): ...
-def read_rich_text(xml_source): ...
--- stubs/openpyxl/utils/bound_dictionary.pyi
+++
@@ -1,7 +0,0 @@
-from _typeshed import Incomplete
-from collections import defaultdict
-
-class BoundDictionary(defaultdict):
-    reference: Incomplete
-    def __init__(self, reference: Incomplete | None = None, *args, **kw) -> None: ...
-    def __getitem__(self, key): ...
